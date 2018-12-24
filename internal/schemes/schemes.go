c379221cc4321f2ebfde0200b2c5b53f58a1deda
Bounteous <bounteous17@protonmail.com>
2018-12-24 20:33:32 +0100

    More global schemes

--- internal/schemes/schemes.go
+++ internal/schemes/schemes.go
@@ -8,11 +8,40 @@ type UserStruct struct {
 	Token    string
 }
 
-// Verbose : verbosity params
+// VerboseStruct : verbosity params
 type VerboseStruct struct {
 	Verbose bool
 	Level   int
 }
 
+// ServerAPIStruct : connect to api
+type ServerAPIStruct struct {
+	Server string
+	Port   int
+}
+
+// TrackingStruct : logging events from client actions
+type TrackingStruct struct {
+	Path string
+	Cli  string
+}
+
+// ConfigStruct : global config sections
+type ConfigStruct struct {
+	ServerAPI ServerAPIStruct
+	Tracking  TrackingStruct
+}
+
+// UserSysStruct : user info
+type UserSysStruct struct {
+	Username string
+	Uid      string
+	GroupIds []string
+	HomeDir  string
+}
+
 var User = &UserStruct{}
 var Verbose = &VerboseStruct{}
+var ServerApi = &ServerAPIStruct{}
+var Config = &ConfigStruct{}
+var UserSys = &UserSysStruct{}
